3088303a0f37f5fbd62bc744d0466a2bcce7d8a1
Padmasree <padmasree.madala@gmail.com>
2024-02-27 07:05:47 +0530

    Salary process Changes by Padma

--- src/app/components/dashboard/comp-list/salaryproces/salaryprocess.component.ts
+++ src/app/components/dashboard/comp-list/salaryproces/salaryprocess.component.ts
@@ -1,10 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject, Optional } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonService } from '../../../../services/common.service';
 import { String } from 'typescript-string-operations';
 import { ApiConfigService } from '../../../../services/api-config.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../../../../services/api.service';
+import { StatusCodes } from '../../../../enums/common/common';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AddOrEditService } from '../add-or-edit.service';
 
 interface Month {
   value: string;
@@ -24,7 +30,8 @@ interface Year {
 export class SalaryProcessComponent implements OnInit {
 
   modelFormData: FormGroup;
-  
+  formData: any;
+  structureList: any;
   displayedColumns: string[] = ['branchCode'];
   dataSource: MatTableDataSource<any>;
 
@@ -48,9 +55,6 @@ export class SalaryProcessComponent implements OnInit {
 
   year : Year[]=
   [
-    { value: '2020', viewValue: '2020' },
-    { value: '2021', viewValue: '2021' },
-    { value: '2022', viewValue: '2022' },
     { value: '2023', viewValue: '2023' },
     { value: '2024', viewValue: '2024' }
   ];
@@ -59,14 +63,24 @@ export class SalaryProcessComponent implements OnInit {
     private formBuilder: FormBuilder,
     private commonService: CommonService,
     private apiConfigService: ApiConfigService,
-
-  ) {
+    public route: ActivatedRoute,
+    private router: Router,
+    private apiService: ApiService,
+    private spinner: NgxSpinnerService,
+    public dialogRef: MatDialogRef<SalaryProcessComponent>,
+    private addOrEditService: AddOrEditService,
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
 
     this.modelFormData = this.formBuilder.group({
-      emp_Code: [''],
+      structureName: [null],
+      // emp_Code: [''],
       sal_Month: [''],
       sal_Year: ['']
     });
+    this.formData = { ...data };
+    if (!this.commonService.checkNullOrUndefined(this.formData.item)) {
+      this.modelFormData.patchValue(this.formData.item);
+    }
   }
 
   checkDates(group: FormGroup) {
@@ -77,13 +91,36 @@ export class SalaryProcessComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.getStructureList();
   }
   
- cancel() {
+  getStructureList() {
+    const getStructureList = String.Join('/', this.apiConfigService.getStructureList);
+    this.apiService.apiGetRequest(getStructureList)
+      .subscribe(
+        response => {
+          const res = response;
+          if (!this.commonService.checkNullOrUndefined(res) && res.status === StatusCodes.pass) {
+            if (!this.commonService.checkNullOrUndefined(res.response)) {
+              this.structureList = res.response['structuresList'];
+            }
+          }
+          this.spinner.hide();
+        });
+  }
 
+ cancel() {
+  this.dialogRef.close();
   }
 
   save() {
+    if (this.modelFormData.invalid) {
+      return;
+    }
+    this.formData.item = this.modelFormData.value;
+    this.addOrEditService[this.formData.action](this.formData, (res) => {
+      this.dialogRef.close(this.formData);
+    });
     
   }
 
