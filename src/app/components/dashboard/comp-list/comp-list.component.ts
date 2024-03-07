b2951ae88e4ab3c762d18f9dc3cbefd8aca6f11c
varun0209 <40246792+varun0209@users.noreply.github.com>
2024-03-07 18:44:01 +0530

    issue

--- src/app/components/dashboard/comp-list/comp-list.component.ts
+++ src/app/components/dashboard/comp-list/comp-list.component.ts
@@ -71,7 +71,7 @@ export class CompListComponent implements OnInit, OnDestroy {
 
   getTableData() {
     let getUrl = ''
-    if(this.tableUrl.url == "MaterialMaster/GetMaterialMasterList" || this.tableUrl.url == "BusienessPartnerAccount/GetBusienessPartnerAccountList") {
+    if(this.tableUrl.url == "MaterialMaster/GetMaterialMasterList" || this.tableUrl.url == "BusienessPartnerAccount/GetBusienessPartnerAccountList" || this.tableUrl.url == "Employee/GetEmployeeList") {
       let obj = JSON.parse(localStorage.getItem("user"));
       getUrl = String.Join('', this.environment.runtimeConfig.serverUrl, this.tableUrl.url,  `/${obj.companyCode}`);
     } else {
