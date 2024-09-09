4d27fa51963a8d9b32c3bb373b912d64cb2e7ece
varun <varun.manteio@gmail.com>
2024-09-09 09:44:56 +0530

    issues

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,6 +11,8 @@ import { AlertService } from '../../services/alert.service';
 import { ApiService } from '../../services/api.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { CommonService } from '../../services/common.service';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -22,7 +24,14 @@ export class LoginComponent implements OnInit {
 
 
   loginForm: FormGroup;
+  form: FormGroup;
+
+  loginUrlData: any;
+
   isSubmitted = false;
+
+  showOtp = false;
+  ramdomNumber = Math.floor(Math.random() * 1000000)
 
   constructor(
     public translate: TranslateService,
@@ -33,6 +42,7 @@ export class LoginComponent implements OnInit {
     private alertService: AlertService,
     private apiService: ApiService,
     private authService: AuthService,
+    private http: HttpClient,
     private commonService: CommonService
   ) {
     commonService.showNavbar.next(false);
@@ -45,6 +55,9 @@ export class LoginComponent implements OnInit {
       password: ['', Validators.required],
       langSelect: ['english', Validators.required]
     });
+    this.form = this.formBuilder.group({
+      otp: ['', Validators.required],
+    });
   }
 
   // Language Preference
@@ -54,12 +67,25 @@ export class LoginComponent implements OnInit {
     this.translate.currentLang = lang;
   }
 
+  onotp() {
+    this.isSubmitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    if (this.form.value.otp != this.ramdomNumber) {
+      this.alertService.openSnackBar('Invalid Otp', Static.Close, SnackBar.error);
+    } else {
+      this.setRoute();
+    }
+  }
+
 
   onSubmit() {
     this.isSubmitted = true;
     if (this.loginForm.invalid) {
       return;
     }
+    this.isSubmitted = false;
     this.loginAPICall();
   }
 
@@ -73,15 +99,34 @@ export class LoginComponent implements OnInit {
           const res = response;
           if (!this.commonService.checkNullOrUndefined(res) && res.status === StatusCodes.pass) {
             if (!this.commonService.checkNullOrUndefined(res.response)) {
-            //  this.getBranchesForUser(res.response['User']);
-              localStorage.setItem('Token', JSON.stringify(res.response['Token']));
-              this.authService.login(res.response.User);
-              this.router.navigate(['dashboard']);
-
+              this.loginUrlData = res.response;
+              this.otpApi();
             }
+          } else {
+            this.spinner.hide();
           }
-          this.spinner.hide();
         });
+  }
+
+  otpApi() {
+    this.getList()
+      .subscribe(
+        response => {
+          this.spinner.hide();
+          this.showOtp = true;
+        });
+  }
+
+  getList(): Observable<any> {
+    const getLoginUrl = `https://dlt.fastsmsindia.com/messages/sendSmsApi?username=AMTpower&password=AMTpower@&drout=3&senderid=AMTHYD&intity_id=1201171169797828072&template_id=1207171644087137963&numbers=${9666756333}&language=en&message=Hello,%20${this.ramdomNumber}%20is%20your%20OTP%20to%20Access%20AMT%20ERP.%20-AMT%20Power%20Transmission`
+    return this.http.get<any>(getLoginUrl);
+  }
+
+  setRoute() {
+    //  this.getBranchesForUser(res.response['User']);
+    localStorage.setItem('Token', JSON.stringify(this.loginUrlData['Token']));
+    this.authService.login(this.loginUrlData.User);
+    this.router.navigate(['dashboard']);
   }
 
   getBranchesForUser(obj) {
